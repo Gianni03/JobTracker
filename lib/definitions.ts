7e245cc8b481fed267beec72856c66c8c5afa972
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2026-01-25 15:02:21 -0300

    restart project

---
+++ lib/definitions.ts
@@ -0,0 +1,36 @@
+export type User = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  avatar: string; // URL to avatar image or initials
+};
+
+export type ApplicationStatus = 'Aplicado' | 'Entrevista' | 'Oferta' | 'Rechazado' | 'Ghosted';
+
+export type Application = {
+  id: string;
+  company: string;
+  role: string;
+  status: ApplicationStatus;
+  interviewStage?: string;
+  date: string; // ISO 8601 format
+  interviewDate?: string;
+  platform: 'LinkedIn' | 'Web de la Empresa' | 'Referido' | 'Indeed' | 'Otro';
+  link: string;
+  salary: {
+    desired: number;
+    expressed?: number;
+    offer?: number;
+  };
+  contact: {
+    name?: string;
+    method?: string;
+  };
+  description: string;
+  notes?: {
+    general?: string;
+    interview?: string;
+  };
+  feedback?: string;
+};
