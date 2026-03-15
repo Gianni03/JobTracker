209b51f982d767be065345853c66e784b8587b52
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2026-03-15 14:57:48 -0300

    feat: Integrate Google Analytics for website tracking.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google'
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] })
 import './globals.css';
@@ -46,6 +47,20 @@ export default function RootLayout({
           {children}
           <Toaster />
         </ThemeProvider>
+        
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XXH598M857"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XXH598M857');
+          `}
+        </Script>
       </body>
     </html>
   );
